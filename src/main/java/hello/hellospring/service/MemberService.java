750f4bb8b57542f8154db8c54c3d18fe66a3b746
Yeji Kim <kimpizzzaa@gmail.com>
2024-01-02 19:52:27 +0900

    회원관리 예제: 회원서비스 개발

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //회원가입
+    public Long join(Member member){
+        //동일한 이름의 회원은 받지 않음
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+
+    private void validateDuplicateMember(Member member) {
+        //Optional<Member> result = memberRepository.findByName(member.getName());
+        //result.ifPresent(m->{   //값이 있으면 동작. optional이라 가능한 동작.
+        //throw new IllegalStateException("이미 존재하는 회원입니다.");
+        //});
+        //이 위 주석처리된 코드와 아래 코드는 완전 동일.
+        //변수를 예외처리하면 자동으로 optional이 되고, optional의 메서드를 메서드체이닝 할수있다.
+        memberRepository.findByName(member.getName())
+            .ifPresent(m->{   //값이 있으면 동작. optional이라 가능한 동작.
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+}
