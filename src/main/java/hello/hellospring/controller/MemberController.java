047b0334f1012405fdefe4f1569db3a9ed6c4f41
Yeji Kim <kimpizzzaa@gmail.com>
2024-01-03 17:12:37 +0900

    회원관리 예제: 회원조회 구현

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -38,5 +41,14 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list (Model model){ //controller는 model을 통해 정보를 가져옴
+        List<Member> members = memberService.findMembers();
+        //model.addAttribute(String name, Object value) value 객체를 name의 이름으로 추가함.
+        //view코드에선 name의 이름으로 value를 사용.
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
